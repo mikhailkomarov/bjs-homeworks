16608a4170207e148ce70101a7da2d38f1a036a4
mikhailkomarov <michaelkomarov1@gmail.com>
2019-11-20 14:33:58 +0300

    first commit Extended-Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,6 @@
+'use strict'
 
+//Задача №1
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -11,20 +13,58 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+    let x1, x2, result; // Переменные хранящие значения x, x1 и x2
+    let x = []; // Инициализация пустого массива
+    let D = Math.pow(b,2) - 4 * a * c; // Формула дискриминанта 
+
+    // Нахождение корней квадратного уравнения и их вычисление
+    if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        x.push(x1,x2);
+        result = `В квадратном уравнении два корня: x₁ = ${x[0]}, x₂ = ${x[1]}`;
+        return x = result;
+    } else if (D === 0) {
+        x1 = -b / (2 * a);
+        x.push(x1);
+        result = `В квадратном уравнении один корень: x = ${x}`;
+        return x = result;
+    } else if (D < 0) {
+        result = 'В квадратном уравнении корней нет т.к. D < 0';
+        return x = result; 
+    }
 }
 
+
+// Задача №2
+
 function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
+    let marks = window.marks.value.split(" ").map(Number).filter((n)=> !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
     window.averageMark.textContent = averageMark;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+    let sum = 0; // Переменная хранящая сумму всех оценок
+    let arrayMarks = []; // Инициализация пустого массива
+    let scoreCounter = 5; // Переменная хранящая ограничение на количество введённых оценок
+
+    // Проверяем, если количество введенных оценок больше 5, то выводим эту информацию в консоль и обрезаем массив
+    if (marks.length > scoreCounter) {
+        console.log(`Количество оценок: ${marks.length}`);
+        arrayMarks = marks.slice(0, scoreCounter);
+    };
+
+    // С помощью цикла проходим по массиву и складываем все оценки в общую сумму
+    for (let i = 0; i < arrayMarks.length; i++) {
+        sum += arrayMarks[i];
+    };
+    return sum / arrayMarks.length; // Вычисляем среднюю оценку
 }
+
+
+
+//Задача №3
 
 function calculateDrinkTask(){
     let name = window.personName.value;
@@ -34,7 +74,15 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let currentDate = new Date(); // Инициализация объекта с текущей датой
+    let age = currentDate.getFullYear() - dateOfBirthday.getFullYear(); // Переменная хранящая (возраст), результат вычислений даты рождения и текущей даты
+    let result;
+
+    if (age >= 18) {
+        result = `Ваш возраст: ${age}. Не желаете ли олд-фэшн, ${name}`;
+        return result;
+    } else {
+        result = `Ваш возраст: ${age}. Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+        return result;
+    }
 }
